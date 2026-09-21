d6a9d58cc6418a7aa340f8d17ee332b4fe063644
yier12121212yie <321660930+yier12121212yie@users.noreply.github.com>
2026-09-21 21:45:19 +0800

    feat: Add Android release signing configuration

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -37,11 +37,18 @@ android {
     }
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("${project.projectDir}/../keystore/release.keystore")
+            storePassword = "studentage2024"
+            keyAlias = "studentage"
+            keyPassword = "studentage2024"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
